9a54e903eb7be5288d5bb77b54574153d401779c
Fikayo <fik4christ@yahoo.com>
2020-04-08 11:37:37 +0100

    First Commit

---
+++ __tests__/queries.js
@@ -0,0 +1,17 @@
+const app = require("../src/server");
+const supertest = require("supertest");
+
+const request = supertest(app);
+
+test("fetch users", async () => {
+  const response = await request
+    .post("/graphql")
+    .send({
+      query: "{ users{ id, name} }",
+    })
+    .set("Accept", "application/json");
+
+  expect(response.status).toBe(200);
+  expect(response.body).toBeInstanceOf(Object);
+  expect(response.body.data.users.length).toEqual(3);
+});
